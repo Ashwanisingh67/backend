b91072fd803287cf25d8b2275f15ade1c1fb5083
Ankush-Kumar-Yadav <ankushyadav8437@gmail.com>
2025-07-24 00:09:49 +0530

    second

--- Config/db.js
+++ Config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 const database=async (req,res) => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
-    res.status(200).send('Database connected successfully');
-    console.log('Database connected successfully');
+    await mongoose.connect(process.env.MONGO_URL)
+      console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
   }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const database = require('./Config/db');
+const CPCT_OPG_route = require('./Router/CPCT_OPG_route');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 8000;
+app.use('/api', CPCT_OPG_route);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
