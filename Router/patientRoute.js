1ffc9b8ac9930d66964687063190be2284dab310
Ashwani singh <ashwanisingh22222@gmail.com>
2025-08-05 00:27:26 +0530

    change

--- Router/patientRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const patientRegistration= require("../Controllers/patientRegisteration")
-const patientLogin = require("../Controllers/patientLogin");
-
-const router = express.Router();
-router.post("/Registration",patientRegistration);
-router.post("/Login",patientLogin);
-
-module.exports=router
