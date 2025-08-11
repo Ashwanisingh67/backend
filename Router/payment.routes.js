4e1445ae60f606d6f34111dbbd5e07a56f68774f
Ashwani singh <ashwanisingh22222@gmail.com>
2025-08-11 23:50:58 +0530

    c

--- Router/payment.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../Controllers/payment.controller');
-
-// Dental service payment routes
-router.post('/dental/create-order', paymentController.createDentalServiceOrder);
-router.post('/dental/verify', paymentController.verifyPayment);
-
-module.exports = router;
--- config/razorpay.config.js
+++
@@ -1,9 +0,0 @@
-const Razorpay = require('razorpay');
-const { RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET } = require('./constants');
-
-const razorpayInstance = new Razorpay({
-  key_id: RAZORPAY_KEY_ID,
-  key_secret: RAZORPAY_KEY_SECRET
-});
-
-module.exports = razorpayInstance;
